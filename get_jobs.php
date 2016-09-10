30b07ba85e8d18ca6b5e579f5487321c9394bbac
Misty Lackie <mistylackie@users.noreply.github.com>
2016-09-10 09:51:33 -0700

    Update get_jobs.php

--- get_jobs.php
+++ get_jobs.php
@@ -1,13 +1,13 @@
 <?php
-set_time_limit(180); //may need to adjust this when lots of jobs
+set_time_limit(180); //May need to adjust this when lots of jobs
 require 'application/Common.php';
 $allJobs = new GetJobs();
 
 //HN parent id of job thread (example 11611867)
 if (!empty($_GET['id']) && ctype_digit($_GET['id'])) {
     $main_id = $_GET['id'];
-	//Loops through json data and adds jobs to db in formatted structure 
-	$jobs = $allJobs->getJobs($main_id);
+    //Loops through json data and adds jobs to db in formatted structure 
+    $jobs = $allJobs->getJobs($main_id);
 } else {
     echo '<h1>Invalid id passed!</h1>';
 }
