11d8481a098fc0eb3616afe049edcac44f187c5b
Misty Lackie <mistylackie@users.noreply.github.com>
2017-08-31 19:58:19 -0700

    Syntax changes

--- application/GetData.php
+++ application/GetData.php
@@ -12,15 +12,22 @@ class GetData
 
     public function categoryCounts($type,$parent_id)
     {
-        $query = "SELECT category, total_count FROM category_counts WHERE type = :type AND parent_id = :pid AND total_count > 14 ".
-                 "ORDER BY total_count DESC LIMIT 10";
+        $query = "SELECT category, 
+	                 total_count 
+		    FROM category_counts 
+		   WHERE type = :type 
+		     AND parent_id = :pid 
+		     AND total_count > 14
+		ORDER BY total_count DESC LIMIT 10";
         $result = $this->db->select($query, array('type' => $type,'pid' => $parent_id));
         return $result;
     }
 
     public function getAllJobs($parent_id)
     {
-        $query = "SELECT * FROM hn_posts WHERE parent_id = :pid LIMIT 700";
+        $query = "SELECT * FROM hn_posts 
+	           WHERE parent_id = :pid 
+		   LIMIT 700";
 	$result = $this->db->select($query, array('pid' => $parent_id));
         return $result;
     }
