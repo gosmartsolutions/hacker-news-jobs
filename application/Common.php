c653e8970d3bcabcc828acacf4ccd83146e9a205
Misty Lackie <mistylackie@users.noreply.github.com>
2017-08-31 19:55:32 -0700

    Update Common.php

--- application/Common.php
+++ application/Common.php
@@ -1,12 +1,13 @@
 <?php
-error_reporting(E_ALL);
-error_reporting(E_ALL & ~E_NOTICE | E_STRICT); // Warns on good coding standards
-ini_set("display_errors", "1");
+
+//error_reporting(E_ALL);
+//error_reporting(E_ALL & ~E_NOTICE | E_STRICT); // Warns on good coding standards
+//ini_set("display_errors", "1");
 require 'Config.php';
 require 'HelperFunctions.php';
 
-//Autoloading stuff which autoloads class files based on class name called
-//This avoids having to add includes for every class file
+// Autoloading stuff which autoloads class files based on class name called
+// This avoids having to add includes for every class file
 spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
