b47467f5c62d383f77433e750d3ee665d6ba3d60
Misty Lackie <mistylackie@users.noreply.github.com>
2017-08-31 19:59:22 -0700

    Update HelperFunctions.php

--- application/HelperFunctions.php
+++ application/HelperFunctions.php
@@ -1,11 +1,12 @@
 <?php
-//Escape HTML entities in a string.
+
+// Escape HTML entities in a string.
 function e($value)
 {
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', false);
 }
 
-//Strips non numeric from a string
+// Strips non numeric from a string
 function n($value)
 {
     return preg_replace('/\D/', '', $value);
